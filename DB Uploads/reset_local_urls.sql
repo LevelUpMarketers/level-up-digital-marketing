7908c85f68c49ef6e29397cf708878e3bb40efe7
LevelUpMarketers <admin@leveluprichmond.com>
2025-07-03 06:54:30 -0500

    commit

--- DB Uploads/reset_local_urls.sql
+++
@@ -1,4 +0,0 @@
--- Reset WordPress URL options for the local environment
-UPDATE wp_9e73ifu2ty_options
-SET option_value = 'https://level-up-digital-marketing.local'
-WHERE option_name IN ('siteurl','home');
